1990dab1679cc8a07dc8d8554deb1b74a7c6c1d8
Robin Vobruba <robin.vobruba@gmail.com>
2012-11-17 09:20:33 +0100

    ex02: initial commit

---
+++ ex02/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+TARGET_DIR="${SCRIPT_DIR}/target"
+RESOURCES_DIR="${SCRIPT_DIR}/src/main/resources"
+EXECUTABLE="${TARGET_DIR}/cg1_ex02"
+
+cd "${SCRIPT_DIR}"
+
+if [ ! -f "${EXECUTABLE}" ]; then
+	./build.sh
+fi
+
+cd "${TARGET_DIR}"
+"${EXECUTABLE}"
+
---
+++ ex02/src/cg1_ex2.cpp
@@ -0,0 +1,32 @@
+/* ----------------------------------------------------------------
+   name:           cg1_ex2.cpp
+   purpose:        projection tutorial
+                   entry point, cg1_ws12/13 assignment 2
+   version:        SKELETON CODE
+   author:         katrin lang
+                   computer graphics
+                   tu berlin
+   ------------------------------------------------------------- */
+
+#include <iostream>
+#ifdef __APPLE__ 
+#include <GLUT/glut.h>
+#elif _WIN32
+#include "win32/glut.h"
+#else
+#include <GL/glut.h>
+#endif
+
+#include "context.h"
+#include "objmodel.h"
+
+int main(int argc, char** argv){
+    
+  // initialize OpenGL context
+  Context::init(argc, argv);
+
+  // start GLUT event loop
+  glutMainLoop();
+  
+  return 0;
+}

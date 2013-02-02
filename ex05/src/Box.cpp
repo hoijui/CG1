880e9434df41c911bb224ee21d7f7727d91e54cb
hoijui <hoijui.quaero@gmail.com>
2013-02-02 12:12:49 +0100

    ex05: reformat Box-Ray intersection code [minor]

--- ex05/src/Box.cpp
+++ ex05/src/Box.cpp
@@ -3,18 +3,19 @@
 
 /**
  * Tests intersection with a ray.
+ * Optimized method. see:
  * @see http://people.csail.mit.edu/amy/papers/box-jgt.pdf
  */
 bool Box::TestIntersection(const Ray& r, float t0, float t1) const {
 
 	float tmin, tmax, tymin, tymax, tzmin, tzmax;
 
-	tmin = (bounds[r.sign[0]].x - r.o.x) * r.inv_d.x;
-	tmax = (bounds[1-r.sign[0]].x - r.o.x) * r.inv_d.x;
-	tymin = (bounds[r.sign[1]].y - r.o.y) * r.inv_d.y;
-	tymax = (bounds[1-r.sign[1]].y - r.o.y) * r.inv_d.y;
+	tmin = (bounds[r.sign[0]].x      - r.o.x) * r.inv_d.x;
+	tmax = (bounds[1 - r.sign[0]].x  - r.o.x) * r.inv_d.x;
 
-	if ( (tmin > tymax) || (tymin > tmax) ) {
+	tymin = (bounds[r.sign[1]].y     - r.o.y) * r.inv_d.y;
+	tymax = (bounds[1 - r.sign[1]].y - r.o.y) * r.inv_d.y;
+	if ((tmin > tymax) || (tymin > tmax)) {
 		return false;
 	}
 	if (tymin > tmin) {
@@ -24,10 +25,9 @@ bool Box::TestIntersection(const Ray& r, float t0, float t1) const {
 		tmax = tymax;
 	}
 
-	tzmin = (bounds[r.sign[2]].z - r.o.z) * r.inv_d.z;
-	tzmax = (bounds[1-r.sign[2]].z - r.o.z) * r.inv_d.z;
-
-	if ( (tmin > tzmax) || (tzmin > tmax) ) {
+	tzmin = (bounds[r.sign[2]].z     - r.o.z) * r.inv_d.z;
+	tzmax = (bounds[1 - r.sign[2]].z - r.o.z) * r.inv_d.z;
+	if ((tmin > tzmax) || (tzmin > tmax)) {
 		return false;
 	}
 	if (tzmin > tmin) {
@@ -37,5 +37,5 @@ bool Box::TestIntersection(const Ray& r, float t0, float t1) const {
 		tmax = tzmax;
 	}
 
-	return ( (tmin < t1) && (tmax > t0) );
+	return ((tmin < t1) && (tmax > t0));
 }
